08605d2533da974db2198397852c0d10a95fb1b8
Julian Lastuvka <julian.lastuvka@gmail.com>
2021-09-10 15:41:27 -0300

    added anual history table and js to display and hidden the different tables

--- static/ver-gastos.js
+++ static/ver-gastos.js
@@ -1,17 +1,47 @@
 
 function bt1() {
     const target = document.querySelector(".logo");
-    target.style.color="red";
+    target.style.color = "red";
 }
 
-function mostrar_diario(){
-    const tabla = document.querySelector(".table-history-container")
-    tabla.style.display= "none";
+// Muestra la tabla de gastos diarios y oculta la de mensuales y anuales.
+function mostrar_diario() {
+
+    const tabla_diarios = document.querySelector(".table-history-container") // <- Tabla gastos diarios.
+    const tabla_mensuales = document.querySelector(".tabla-gastos-mensuales-container")
+    const tabla_anuales = document.querySelector(".tabla-gastos-anuales-container")
+
+    tabla_mensuales.style.display = "none";
+    tabla_anuales.style.display = "none";
+    tabla_diarios.style.display = "block";
+
+}
+
+function mostrar_mensual() {
+
+    const tabla_diarios = document.querySelector(".table-history-container") // <- Tabla gastos diarios.
+    const tabla_mensuales = document.querySelector(".tabla-gastos-mensuales-container")
+    const tabla_anuales = document.querySelector(".tabla-gastos-anuales-container")
+
+    tabla_mensuales.style.display = "block";
+    tabla_anuales.style.display = "none";
+    tabla_diarios.style.display = "none";
+}
+
+function mostrar_anual() {
+    const tabla_diarios = document.querySelector(".table-history-container") // <- Tabla gastos diarios.
+    const tabla_mensuales = document.querySelector(".tabla-gastos-mensuales-container")
+    const tabla_anuales = document.querySelector(".tabla-gastos-anuales-container")
+
+    tabla_mensuales.style.display = "none";
+    tabla_anuales.style.display = "block";
+    tabla_diarios.style.display = "none";
 }
 
 function listen() {
 
-    document.querySelector("#btn-anuales").onclick = bt1;
+    document.querySelector("#btn-anuales").onclick = mostrar_anual;
+    document.querySelector("#btn-mensuales").onclick = mostrar_mensual;
     document.querySelector("#btn-diarios").onclick = mostrar_diario;
 }
 
